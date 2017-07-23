72db775660c22f6168cbf4f1893524c804e2d012
drummersbrother <hb11002@icloud.com>
2017-07-23 21:30:44 +0200

    Manually document the constants
    Signed-off-by: drummersbrother <hb11002@icloud.com>

--- docs/reference.rst
+++ docs/reference.rst
@@ -18,3 +18,55 @@ The Exceptions
 
 The Constants
 =============
+
+.. module:: rocket_snake.constants
+
+This module defines constants that are used when requesting information from the API.
+
+.. note:: The value of these should not be hardcoded in your code, since these might change at any time.
+
+These constants are all uppercase.
+Here is the rundown:
+
+=================== ========================================================================================================
+Platform related constants
+----------------------------------------------------------------------------------------------------------------------------
+Name                Description
+=================== ========================================================================================================
+``STEAM``           This is the string that represents Steam as a platform.
+``PS4``             ^ But for Playstation 4.
+``XBOX1``           ^ But for Xbox One.
+``ALL_PLATFORMS``   A :class:`set` of the previous platform strings.
+``STEAM_ID``        This is the ID of the steam platform string.
+``PS4_ID``          ^ But for Playstation 4.
+``XBOX1_ID``        ^ But for Xbox One.
+``ALL_IDS``         A :class:`set` of the previous platform IDs.
+``ID_PLATFORM_LUT`` A :class:`dict` with the members of ``ALL_IDS`` as keys and the members of ``ALL_PLATFORMS`` as values.
+``PLATFORM_ID_LUT`` The inverse of ``ID_PLATFORM_LUT``, that is, platforms as keys and IDs as values.
+=================== ========================================================================================================
+
+======================= ===================================================================================
+Leaderboard related constants
+-----------------------------------------------------------------------------------------------------------
+Name                    Description
+======================= ===================================================================================
+``LEADERBOARD_WINS``    This is the string that represents a leaderboard based/filtered on number of wins.
+``LEADERBOARD_GOALS``   ^ But for number of goals.
+``LEADERBOARD_MVPS``    ^ But for number of MVPs.
+``LEADERBOARD_SAVES``   ^ But for number of saves.
+``LEADERBOARD_SHOTS``   ^ But for number of shots.
+``LEADERBOARD_ASSISTS`` ^ But for number of assists
+``LEADERBOARD_TYPES``   A :class:`set` of all the previous leaderboard filter types.
+======================= ===================================================================================
+
+=========================== =====================================================
+Platform related constants
+---------------------------------------------------------------------------------
+Name                        Description
+=========================== =====================================================
+``RANKED_DUEL_ID``          This is the ID of the ranked duels playlist.
+``RANKED_DOUBLES_ID``       ^ But for the ranked doubles playlist.
+``RANKED_SOLO_STANDARD_ID`` ^ But for the ranked solo standard playlist.
+``RANKED_STANDARD_ID``      ^ But for the ranked standard playlist.
+``RANKED_PLAYLISTS_IDS``    A :class:`set` of all the previous playlist IDs.
+=========================== =====================================================
--- rocket_snake/constants.py
+++ rocket_snake/constants.py
@@ -14,34 +14,34 @@
    limitations under the License.
 """
 
-#: Just some constants that are used
-STEAM = "Steam" #: Steam platform as a string
-PS4 = "PS4" #: Playstation 4 platform as a string
-XBOX1 = "XboxOne" #: Xbox One platform as a string
+# Just some constants that are used
+STEAM = "Steam" # Steam platform as a string
+PS4 = "PS4" # Playstation 4 platform as a string
+XBOX1 = "XboxOne" # Xbox One platform as a string
 
-ALL_PLATFORMS = {STEAM, PS4, XBOX1} #: A set of all the platforms, useful for membership tests
+ALL_PLATFORMS = {STEAM, PS4, XBOX1} # A set of all the platforms, useful for membership tests
 
-STEAM_ID = 1 #: Platform id for the Steam platform
-PS4_ID = 2 #: Platform id for the Playstation 4 platform
-XBOX1_ID = 3 #: Platform id for the Xbox One platform
+STEAM_ID = 1 # Platform id for the Steam platform
+PS4_ID = 2 # Platform id for the Playstation 4 platform
+XBOX1_ID = 3 # Platform id for the Xbox One platform
 
-ALL_IDS = {STEAM_ID, PS4_ID, XBOX1_ID} #: A set of all the platform IDs, useful for membership tests
-ID_PLATFORM_LUT = {STEAM_ID: STEAM, PS4_ID: PS4, XBOX1_ID: XBOX1} #: A dict with platform IDs as keys as platform strings as values
-PLATFORM_ID_LUT = {STEAM: STEAM_ID, PS4: PS4_ID, XBOX1: XBOX1_ID} #: A dict with platform strings as keys and platform IDs as values
+ALL_IDS = {STEAM_ID, PS4_ID, XBOX1_ID} # A set of all the platform IDs, useful for membership tests
+ID_PLATFORM_LUT = {STEAM_ID: STEAM, PS4_ID: PS4, XBOX1_ID: XBOX1} # A dict with platform IDs as keys as platform strings as values
+PLATFORM_ID_LUT = {STEAM: STEAM_ID, PS4: PS4_ID, XBOX1: XBOX1_ID} # A dict with platform strings as keys and platform IDs as values
 
-LEADERBOARD_WINS = "wins" #: The stat leaderboard type to filter by number of wins
-LEADERBOARD_GOALS = "goals" #: The stat leaderboard type to filter by number of goals
-LEADERBOARD_MVPS = "mvps" #: The stat leaderboard type to filter by number of MVPs
-LEADERBOARD_SAVES = "saves" #: The stat leaderboard type to filter by number of saves
-LEADERBOARD_SHOTS = "shots" #: The stat leaderboard type to filter by number of shots
-LEADERBOARD_ASSISTS = "assists" #: The stat leaderboard type to filter by number of assists
+LEADERBOARD_WINS = "wins" # The stat leaderboard type to filter by number of wins
+LEADERBOARD_GOALS = "goals" # The stat leaderboard type to filter by number of goals
+LEADERBOARD_MVPS = "mvps" # The stat leaderboard type to filter by number of MVPs
+LEADERBOARD_SAVES = "saves" # The stat leaderboard type to filter by number of saves
+LEADERBOARD_SHOTS = "shots" # The stat leaderboard type to filter by number of shots
+LEADERBOARD_ASSISTS = "assists" # The stat leaderboard type to filter by number of assists
 
 LEADERBOARD_TYPES = {LEADERBOARD_WINS, LEADERBOARD_GOALS, LEADERBOARD_MVPS,
-                     LEADERBOARD_SAVES, LEADERBOARD_SHOTS, LEADERBOARD_ASSISTS} #: A set of all the types of leaderboards, useful for looping over.
+                     LEADERBOARD_SAVES, LEADERBOARD_SHOTS, LEADERBOARD_ASSISTS} # A set of all the types of leaderboards, useful for looping over.
 
-RANKED_DUEL_ID = 10 #: The ID of the ranked duels playlist
-RANKED_DOUBLES_ID = 11 #: The ID of the ranked doubles playlist
-RANKED_SOLO_STANDARD_ID = 12 #: The ID of the ranked solo standard playlist
-RANKED_STANDARD_ID = 13 #: The ID of the ranked standard playlist
+RANKED_DUEL_ID = 10 # The ID of the ranked duels playlist
+RANKED_DOUBLES_ID = 11 # The ID of the ranked doubles playlist
+RANKED_SOLO_STANDARD_ID = 12 # The ID of the ranked solo standard playlist
+RANKED_STANDARD_ID = 13 # The ID of the ranked standard playlist
 
-RANKED_PLAYLISTS_IDS = {RANKED_DUEL_ID, RANKED_DOUBLES_ID, RANKED_SOLO_STANDARD_ID, RANKED_STANDARD_ID} #: A set of all the ranked playlist IDs, useful for looping or membership tests
+RANKED_PLAYLISTS_IDS = {RANKED_DUEL_ID, RANKED_DOUBLES_ID, RANKED_SOLO_STANDARD_ID, RANKED_STANDARD_ID} # A set of all the ranked playlist IDs, useful for looping or membership tests
